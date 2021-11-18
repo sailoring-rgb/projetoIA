c5ee072a686d6a859b37dd5a6ac898511ced4e0d
Ana Henriques <62114404+AnaHenriques01@users.noreply.github.com>
2021-11-18 00:30:00 +0000

    predicado estafetaMaisEcologico

--- funcionalidades.pl
+++ funcionalidades.pl
@@ -1,11 +1,32 @@
 :- consult('baseConhecimento.pl').
 :- consult('aux.pl').
 
-%Funcionalidades
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
 
+:- style_check(-singleton).
+
+%Funcionalidades
+		
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado estafetaMaisEcologico: Lista,Maximo,Id -> {V,F}
+% Identifica o estafeta que utilizou mais vezes um meio de transporte mais ecológico
+
+estafetaMaisEcologico([],0,_).
+
+estafetaMaisEcologico([IdEstaf],Max,IdEstaf) :-
+	encomendasEcologicasCount(IdEstaf,Count),
+	Max = Count.
+	
+estafetaMaisEcologico([IdEstaf|T],Max,IdMax) :-
+	encomendasEcologicasCount(IdEstaf,Count),
+	estafetaMaisEcologico(T,CountMax,Id),
+	(Count > CountMax -> Max = Count, IdMax = IdEstaf;
+	 Max = CountMax, IdMax = Id).
+	 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado estafetasEncomendasCliente: Id,Lista,Resultado -> {V,F}
-% Identificar que estafetas entregaram determinada(s) encomenda(s) a um determinado cliente
+% Identifica que estafetas entregaram determinada(s) encomenda(s) a um determinado cliente
 
 estafetasEncomendasCliente([IdEnc|Es]) :- 
 	estafetasEncCliente(IdEnc,R),
