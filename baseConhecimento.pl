f15d4cd3f9f77f63f271c59d981b9105675b06f7
Ana Henriques <62114404+AnaHenriques01@users.noreply.github.com>
2021-11-18 13:16:21 +0000

    correção de erro em baseConhecimento.pl

--- baseConhecimento.pl
+++ baseConhecimento.pl
@@ -3,21 +3,23 @@
 
 
 encomenda(1,1,30,20,'6h','Carro').
-encomenda(3,2,5,30,'2h','Bicleta').
-encomenda(4,3,10,'3h','Mota').
+encomenda(2,4,2,15,'Imediato','Bicicleta').
+encomenda(3,2,5,30,'2h','Bicicleta').
+encomenda(4,3,10,25,'3h','Mota').
 
 %---------------Cliente------------------
 % Cliente: #IdCliente, TempoMax, Nota 
 
 cliente(1,'6h',4).
-cliente(2,'Imediato',3.5).
-cliente(3,'1 dia', 5).
+cliente(2,'2h',3.5).
+cliente(3,'3h', 5).
+cliente(3,'Imediato', 2).
 
 %--------------Estafeta------------------
 % Estafeta: #IdEstf, #IdEnc, Rua, Freguesia
 
 estafeta(1,1,'Rua da Boavista','Real').
 estafeta(2,1,'Rua da Boavista','Real').
-estafeta(1,2,'Rua da Veiga','Dume').
-estafeta(2,3,'Rua da Universidade','Gualtar').
-
+estafeta(2,2,'Av. Robert Smith','Lamaçães').
+estafeta(3,2,'Rua da Veiga','Dume').
+estafeta(3,3,'Rua da Universidade','Gualtar').
