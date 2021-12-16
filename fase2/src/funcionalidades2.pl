76ce987ca9e1689f6596a15c33a94064b09ddb9a
Ana Henriques <pbapohenriques@hotmail.com>
2021-12-16 12:29:21 +0000

    funcionalidades2.pl

--- fase2/src/funcionalidades2.pl
+++ fase2/src/funcionalidades2.pl
@@ -1,7 +1,9 @@
 :- consult('baseConhecimento.pl').
-:- consult('auxiliares1.pl').
+:- consult('auxiliares2.pl').
 :- consult('invariantes.pl').
 :- consult('evolucaoInvolucao.pl').
+
+:- set_prolog_stack(global, limit(100000000000)).
 
 %-------------------------------Pesquisa em Profundidade (DFS)-------------------------------
 
@@ -10,6 +12,7 @@ Se o estafeta realizar mais do que uma entrega num percurso,
 tem-se de ter em atenção o transporte que ele usa e a quantidade de peso que ele transporta.
 */
 resolve_DFS(Nodo,[Nodo|Caminho],Distancia) :-
+    g(Grafo),
     profundidade(Grafo,Nodo,[Nodo],Caminho,Distancia).
 
 profundidade(Grafo,Nodo,_,[],0) :- goal(Nodo).
@@ -18,3 +21,12 @@ profundidade(Grafo,Nodo,Historico,[ProxNodo|Caminho],DistanciaT) :-
     nao(membro(ProxNodo,Historico)),
     profundidade(ProxNodo,[ProxNodo|Historico],Caminho,Distancia2),
     DistanciaT is Distancia1 + Distancia2.
+
+statistics_DFS() :-
+    statistics(global_stack, [G1,L1]),
+    time(resolve_DFS('Dume',_,D)),
+    statistics(global_stack, [G2,L2]),
+    Res is G2 - G1,
+    write("Memory: "), 
+    write(Res),write("\n"),
+	write("Custo: "),write(D).
